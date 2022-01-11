a35f1f4d53434a81ebf9c1af3450191261aac53d
Noah2211 <noah.decroix3@gmail.com>
2022-01-11 22:15:36 +0100

    done

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
 // Premiere ligne
+$today = date("D");
+echo $today."<br>";
+
+
+$date = date("Y.m.d", mktime(0,0,0,12,10,2018));
+echo $date ."<br>";
+
+$hour = date('h:i:s' , mktime(11, 35, 07));
+echo $hour;
 
 
 
